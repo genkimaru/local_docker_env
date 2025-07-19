fc024ef05d08249f8da09702aaa4327c3215545d
vonguan <vonguan@163.com>
2025-07-20 01:12:26 +0900

    chore: remove obsolete directories from previous architecture

--- wildfly-hulft-agent/hulft8-installer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Placeholder for Hulft 8 installation steps
-
-echo "[INFO] Hulft 8 installation is not automated. Please obtain the installer from the official source and install manually."
-echo "[INFO] See: https://www.hulft.com/en/ for more information."
-# Actual installation commands would go here
