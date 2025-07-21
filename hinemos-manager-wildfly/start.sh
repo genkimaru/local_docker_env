d1e96627b11e10d48bda058d46fb4bc522367ab6
vonguan <vonguan@163.com>
2025-07-21 16:46:56 +0900

    modify /opt/hinemos

--- hinemos-manager-wildfly/start.sh
+++ hinemos-manager-wildfly/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Start Hinemos Manager and Web as hinemos user
-su - hinemos -c "/opt/hinemos/bin/hinemos_manager start"
-su - hinemos -c "/opt/hinemos/bin/hinemos_web start"
+# Start Hinemos Manager as hinemos-manager user
+su - hinemos-manager -c "/opt/hinemos-manager/bin/hinemos_manager start"
+# Start Hinemos Web as hinemos-web user
+su - hinemos-web -c "/opt/hinemos-web/bin/hinemos_web start"
 
 # Start WildFly as wildfly user
 su - wildfly -c "/opt/wildfly/bin/standalone.sh -b 0.0.0.0 &"
